ba12a16c07e37c1bfa069fad1897e910722e946f
William Axel Cuangdinata <williamaxelcuangdinata@gmail.com>
2026-05-27 16:15:20 +0000

    fix: registerSW 404 error from Vite PWA injector

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      injectRegister: 'auto',
+      injectRegister: false,
       includeAssets: ['favicon.svg', 'icons.svg'],
       manifest: {
         name: 'MRelay',
